492a44f404a036ab4e7c68dabd904324f35bcba1
Sarah John <smj5860@psu.edu>
2023-01-28 11:48:25 -0500

    TA-Sarah

--- src/Arrow.java
+++ src/Arrow.java
@@ -1,7 +1,7 @@
 public class Arrow extends Weapon{
 
     public Arrow() {
-        super("Arrow", "An Arrow. Sharp and pointed at one end and often has feathers at the other end ",2,1);
+        super("Arrow", "An Arrow. Sharp and pointed at one end and often has feathers at the other end ",5,15);
     }
 }
 
