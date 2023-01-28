5a57e4009c76e33833781fd068999ae4fe7d686f
Sarah John <smj5860@psu.edu>
2023-01-28 11:43:20 -0500

    TA-Sarah

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-            Item i1 = new Item(  "item name", "item desc",  10);
-            Item i2 = new Item("item name2", "item desc2", 11);
+
             Weapon w1 = new Weapon("weapon name","weapon description", 10, 50);
             Gold goldBar = new Gold(10);
             Sword magicSword = new Sword();
